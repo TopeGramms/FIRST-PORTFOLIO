a47beb7299e67ead127a51ad780b7839bcf80d51
Adekalu Temitope <106330123+TopeGramms@users.noreply.github.com>
2024-07-06 03:23:13 +0100

    Saturday

--- script.js
+++ script.js
@@ -1,6 +1,27 @@
+// toogleMenu
 function toggleMenu() {
     const menu = document.querySelector(".menu-links");
     const icon = document.querySelector(".hamburger-icon");
     menu.classList.toggle("open");
     icon.classList.toggle("open");
+}
+// loading
+document.addEventListener("DOMContentLoaded", function() {
+    const loader = document.getElementById("loader");
+    const content = document.getElementById("content");
+
+    // Simulate loading time
+    setTimeout(function () {
+        loader.style.display = "none";
+        content.style.display = "block";
+    }, 4000);
+    
+});
+// Messages
+function submitMessages() {
+    const message1 = document.getElementById('messageInput1').value;
+    const message2 = document.getElementById('messageInput2').value;
+    const message3 = document.getElementById('messageInput3').value;
+    alert(`Messages submitted:\n1. ${message1}\n2. ${message2}\n3. ${message3}`);
+    // Add any further processing logic here
 }
